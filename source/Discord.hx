eddd50c5099a567ea14fd6a9e34563c994469d60
OniSki <MoikCVIktor@gmail.com>
2022-02-21 02:30:00 -0300

    New Discord Rich Presence

--- source/Discord.hx
+++ source/Discord.hx
@@ -11,7 +11,7 @@ class DiscordClient
 	{
 		trace("Discord Client starting...");
 		DiscordRpc.start({
-			clientID: "863222024192262205",
+			clientID: "944815048776351785",
 			onReady: onReady,
 			onError: onError,
 			onDisconnected: onDisconnected
@@ -39,7 +39,7 @@ class DiscordClient
 			details: "In the Menus",
 			state: null,
 			largeImageKey: 'icon',
-			largeImageText: "Psych Engine"
+			largeImageText: "Editor's Cut"
 		});
 	}
 
